8a585fa05c9299b01ea1fed55eb4163e2c0bfd16
Robert Horvick <robert.horvick@gmail.com>
2026-02-27 11:02:19 -0500

    Update dashboard.sh to specify host and port for uvicorn. Change permissions of pg.sh to make it executable.

--- dashboard.sh
+++ dashboard.sh
@@ -6,7 +6,7 @@ set -e
 # if the service is running on 8000, kill it hard
 lsof -ti :8000 | xargs kill -9
 
-uv run uvicorn src.api.main:app --reload &
+uv run uvicorn src.api.main:app --reload --host 0.0.0.0 --port 8000 &
 
 # wait for server to respond
 until curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8000/dashboard | grep -q 200; do sleep 0.5; done
